4f208838a3119250283c9e75fe40f48d2dae0e0d
iollar <posy2000@mail.ru>
2022-08-03 17:04:32 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Курсач
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comm = new System.Windows.Forms.TextBox();
+            this.v1 = new System.Windows.Forms.TextBox();
+            this.v2 = new System.Windows.Forms.TextBox();
+            this.create = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clean = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.Color.Honeydew;
+            this.picture.Location = new System.Drawing.Point(12, 147);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(517, 500);
+            this.picture.TabIndex = 3;
+            this.picture.TabStop = false;
+            // 
+            // search
+            // 
+            this.search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.search.Location = new System.Drawing.Point(776, 450);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(175, 108);
+            this.search.TabIndex = 5;
+            this.search.Text = "найти кратчайший путь";
+            this.search.UseVisualStyleBackColor = false;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(561, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(390, 337);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // comm
+            // 
+            this.comm.Enabled = false;
+            this.comm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.comm.Location = new System.Drawing.Point(12, 4);
+            this.comm.Multiline = true;
+            this.comm.Name = "comm";
+            this.comm.Size = new System.Drawing.Size(517, 137);
+            this.comm.TabIndex = 9;
+            // 
+            // v1
+            // 
+            this.v1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.v1.Location = new System.Drawing.Point(688, 400);
+            this.v1.Name = "v1";
+            this.v1.Size = new System.Drawing.Size(51, 35);
+            this.v1.TabIndex = 10;
+            this.v1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // v2
+            // 
+            this.v2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.v2.Location = new System.Drawing.Point(889, 400);
+            this.v2.Name = "v2";
+            this.v2.Size = new System.Drawing.Size(51, 35);
+            this.v2.TabIndex = 11;
+            this.v2.TextChanged += new System.EventHandler(this.v2_TextChanged);
+            // 
+            // create
+            // 
+            this.create.BackColor = System.Drawing.Color.Khaki;
+            this.create.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.create.Location = new System.Drawing.Point(561, 450);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(196, 108);
+            this.create.TabIndex = 12;
+            this.create.Text = "построить тропы";
+            this.create.UseVisualStyleBackColor = false;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(632, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 29);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Матрица смежности";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(579, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "начало:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(794, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 29);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "конец:";
+            // 
+            // clean
+            // 
+            this.clean.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clean.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.clean.Location = new System.Drawing.Point(561, 564);
+            this.clean.Name = "clean";
+            this.clean.Size = new System.Drawing.Size(390, 83);
+            this.clean.TabIndex = 17;
+            this.clean.Text = "очистить всё";
+            this.clean.UseVisualStyleBackColor = false;
+            this.clean.Click += new System.EventHandler(this.clean_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 659);
+            this.Controls.Add(this.clean);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.v2);
+            this.Controls.Add(this.v1);
+            this.Controls.Add(this.comm);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.picture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "поиск кратчайшего пути";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox comm;
+        private System.Windows.Forms.TextBox v1;
+        private System.Windows.Forms.TextBox v2;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clean;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            MatrixAdg(KolvoV);
+            comm.Text = "Заполните матрицу смежности, указывая длины троп, соединяющих площадки. Нажмите кнопку \"построить тропы\".";
+            comm.Text += "\r\nВНИМАНИЕ! Если между площадками имеется несколько кратчайших путей, то изображен будет только один.";
+        }
+        const int KolvoV = 7;
+
+        float[,] MasXY = new float[100, 100];
+        float[,] MasOD = new float[100, 100];
+        
+
+        //строим вершины
+        private void VertexOnArc(Graphics DrawingArea, float centerX, float centerY, float radius, float angle1, float angle2, int val)
+        {
+            Font font = new Font("Arial", 14, FontStyle.Regular);
+            Pen pen = new Pen(Color.Green,5);
+
+            angle1 = (float)((angle1 / 180) * Math.PI);
+            angle2 = (float)((angle2 / 180) * Math.PI);
+            float delta = (angle2 - angle1) / val;
+
+            float x1 = centerX + radius * (float)Math.Cos(angle1);
+            float y1 = centerY - radius * (float)Math.Sin(angle1);
+            MasXY[0, 0] = x1;
+            MasXY[1, 0] = y1;
+            for (int i = 1; i <= val; i++)
+            {
+                angle1 += delta;
+                float x2 = centerX + radius * (float)Math.Cos(angle1);
+                float y2 = centerY - radius * (float)Math.Sin(angle1);
+                DrawingArea.DrawEllipse(pen, x1 - 15, y1 - 15, 30, 30);
+                DrawingArea.FillEllipse(Brushes.White, x1 - 15, y1 - 15, 30, 30);
+                DrawingArea.DrawString(i.ToString(), font, Brushes.Black, x1 - 8, y1 - 10);
+                MasXY[0, i] = x2;
+                MasXY[1, i] = y2;
+                x1 = x2;
+                y1 = y2;
+            }
+        }
+        
+
+        //матрица смежности
+        private void MatrixAdg(int val)
+        {
+            dataGridView1.Columns.Clear();
+            for (int i = 1; i <= val; i++)
+            {
+                dataGridView1.Columns.Add(i.ToString(), "v" + i.ToString());
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i - 1].HeaderCell.Value = "v" + i.ToString(); //
+                dataGridView1.Columns[i - 1].Width = 30;
+                dataGridView1.RowHeadersWidth = 48;
+            }
+            for (int i = 0; i < val; i++)
+                for (int j = 0; j < val; j++)
+                    if (i == j)
+                        dataGridView1[i, j].Value = "0";
+                    else
+                        dataGridView1[i, j].Value = "\u221e";
+        }
+
+
+        //симметричное заполнение матрицы смежности
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                dataGridView1[e.RowIndex, e.ColumnIndex].Value = dataGridView1[e.ColumnIndex, e.RowIndex].Value;
+            }
+            catch
+            { }
+        }
+
+
+        //создать граф
+        private void create_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            Graphics pic = Graphics.FromImage(bmp);
+            picture.Image = bmp;
+
+            VertexOnArc(pic, picture.Width / 2, picture.Height / 2, 140, 450, 90, KolvoV);
+
+            int val = KolvoV;
+            float x1, x2, y1, y2;
+
+            {
+                Pen pen = new Pen(Color.DarkKhaki, 3);
+                for (int i = 0; i < val; i++)
+                {
+                    for (int j = i + 1; j < val; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value.ToString() != "\u221e")
+                        {
+                            x1 = MasXY[0, i];
+                            y1 = MasXY[1, i];
+                            x2 = MasXY[0, j];
+                            y2 = MasXY[1, j];
+                            float theta = (float)Math.Atan2(y2 - y1, x2 - x1);
+                            x1 = x1 + 15 * (float)Math.Cos(theta);
+                            y1 = y1 + 15 * (float)Math.Sin(theta);
+                            x2 = x2 - 15 * (float)Math.Cos(theta);
+                            y2 = y2 - 15 * (float)Math.Sin(theta);
+                            pic.DrawLine(pen, x1, y1, x2, y2);
+                        }
+                    }
+                }
+            }
+            comm.Text = "Укажите номера начальной и конечной площадки. Нажмите кнопку \"найти кратчайший путь\".\r\n";
+
+        }
+        
+
+        // Алгоритм Дейкстры
+        List<List<int>> way = new List<List<int>>();
+        string[] length = new string[KolvoV];
+        bool testG = false;
+        private void Dijkstra(int v1, int v2)
+        {
+            try
+            {
+                HashSet<int> Clean = new HashSet<int>();
+                List<int> wayNow = new List<int>();
+                for (int i = 0; i < KolvoV; i++)
+                { }
+
+                //первоначальное заполнение
+                for (int i = 0; i < KolvoV; i++)
+                {
+                    if (i != v1)
+                    {
+                        length[i] = "\u221e";
+                    }
+                    else
+                    {
+                        length[i] = "0";
+                    }
+                    way.Add(wayNow.GetRange(0, wayNow.Count));
+                    Clean.Add(i);
+                }
+
+
+                bool test = true;
+                int now = 0;
+
+                //проверка на не посещенность            
+                for (int k = 0; k < 7; k++)
+                {
+                    //выбор вершины с мин меткой
+                    for (int i = 0; i < KolvoV && k != KolvoV; i++)
+                    {
+                        if (Clean.Contains(i) && length[i] != "\u221e" && (test == true || int.Parse(length[i]) < int.Parse(length[now])))
+                        {
+                            test = false;
+                            now = i;
+                            wayNow = way[now];
+                            wayNow.Add(now);
+                        }
+                    }
+
+                    //проверка смежных с now, новая метка для i
+                    for (int i = 0; i < KolvoV; i++)
+                        if (
+                               Clean.Contains(i) &&
+                               dataGridView1.Rows[now].Cells[i].Value.ToString() != "\u221e" &&
+                               (length[i] == "\u221e" ||
+                               int.Parse(dataGridView1.Rows[now].Cells[i].Value.ToString()) + int.Parse(length[now]) < int.Parse(length[i]))
+                           )
+                        {
+                            length[i] = (uint.Parse(dataGridView1.Rows[now].Cells[i].Value.ToString()) + int.Parse(length[now])).ToString();
+
+                            way[i] = wayNow.GetRange(0, wayNow.Count);
+                        }
+                    Clean.Remove(now);
+                    test = true;
+                }
+                testG = true;
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Недопустимое значение! Длина тропы должна являться неотрицательным числом от 0 до 4 294 967 295");
+                testG = false;
+            }
+            catch (System.OverflowException)
+            {
+                MessageBox.Show("Недопустимое значение! Длина тропы должна являться неотрицательным числом от 0 до 4 294 967 295");
+                testG = false;
+            }
+        }
+
+        //построение мин. пути
+        int vertex1 = 0, vertex2 = 0;
+        private void search_Click(object sender, EventArgs e)
+        {
+            if (v1.Text == "" || v2.Text == "")
+                MessageBox.Show("Вы забыли внести номера площадок");
+            else
+            {
+                int val = KolvoV;
+                try
+                {
+                    vertex1 = Int32.Parse(v1.Text);
+                    vertex2 = Int32.Parse(v2.Text);
+
+                    if (vertex1 > 0 && vertex1 < 8 && vertex2 < 8 && vertex2 > 0)
+                    {
+                        Dijkstra(vertex1 - 1, vertex2 - 1);
+
+                        if (length[vertex2 - 1] == "\u221e" && testG)
+                            comm.Text = "Пути от площадки" + vertex1 + " до площадки " + vertex2 + " не существует";
+                        else
+                        if(testG)
+                        {
+                            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+                            Graphics pic = Graphics.FromImage(bmp);
+                            picture.Image = bmp;
+                            VertexOnArc(pic, picture.Width / 2, picture.Height / 2, 140, 450, 90, KolvoV);
+
+                            float x1, x2, y1, y2;
+
+                            Pen pen = new Pen(Color.DarkKhaki, 3);
+                            Pen pen2 = new Pen(Color.DarkBlue, 6);
+                            for (int i = 0; i < val; i++)
+                            {
+                                for (int j = i + 1; j < val; j++)
+                                {
+                                    if (dataGridView1.Rows[i].Cells[j].Value.ToString() != "\u221e")
+                                    {
+                                        x1 = MasXY[0, i];
+                                        y1 = MasXY[1, i];
+                                        x2 = MasXY[0, j];
+                                        y2 = MasXY[1, j];
+                                        float theta = (float)Math.Atan2(y2 - y1, x2 - x1);
+                                        x1 = x1 + 15 * (float)Math.Cos(theta);
+                                        y1 = y1 + 15 * (float)Math.Sin(theta);
+                                        x2 = x2 - 15 * (float)Math.Cos(theta);
+                                        y2 = y2 - 15 * (float)Math.Sin(theta);
+                                        pic.DrawLine(pen, x1, y1, x2, y2);
+                                    }
+                                }
+                            }
+                            for (int i = 0; i < way[vertex2 - 1].Count - 1; i++)
+                            {
+                                x1 = MasXY[0, way[vertex2 - 1][i]];
+                                y1 = MasXY[1, way[vertex2 - 1][i]];
+                                x2 = MasXY[0, way[vertex2 - 1][i + 1]];
+                                y2 = MasXY[1, way[vertex2 - 1][i + 1]];
+                                float theta = (float)Math.Atan2(y2 - y1, x2 - x1);
+                                x1 = x1 + 15 * (float)Math.Cos(theta);
+                                y1 = y1 + 15 * (float)Math.Sin(theta);
+                                x2 = x2 - 15 * (float)Math.Cos(theta);
+                                y2 = y2 - 15 * (float)Math.Sin(theta);
+                                pic.DrawLine(pen2, x1, y1, x2, y2);
+                            }
+                            comm.Text = "Кратчайший путь от площадки " + vertex1 + " до площадки " + vertex2 + " построен и равен " + length[vertex2 - 1];
+                        }
+                    }
+                    else
+                        MessageBox.Show("Вы ввели несуществующие номера площадок");
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Недопустимое значение! Номер площадки должен являться неотрицательным числом");
+                }
+            }
+            way.Clear();
+        }
+
+
+        private void clean_Click(object sender, EventArgs e)
+        {
+            Process.Start(Application.ExecutablePath);
+            Close();
+        }
+
+
+        //не исп.
+        private void picture_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void v2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+    }
+
+
+}
